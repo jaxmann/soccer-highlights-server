693e7c6ca2952ec1d28d27639e0b89cf761eb8cb
Jonathan Axmann <1314940@NOAM.TCS.com>
2016-12-01 14:15:15 -0500

    initial structure/TODO setup

---
+++ src/main.java
@@ -0,0 +1,25 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//debug flag for print statements
+		
+		//set up favorite teams/players - players and teams you want to track
+		
+		//parse play - figure out what happened - who scored, what team, what the score is, the date?, what sport, set time as NOW
+			
+		//identify a search query based on the play/identify keywords used to search, make sure post is recent (PMR)
+		
+		//perform search on websites, save url, verify its a smallish video file
+		
+		//send a link (create an alert at first)
+		
+		
+		
+		
+	}
+
+
+}
