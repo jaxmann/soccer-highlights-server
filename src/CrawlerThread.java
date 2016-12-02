d69ddb7e34fbbaced82b306f103aa57a47d90d7a
Jonathan Axmann <jonathan.axmann09@gmail.com>
2016-12-02 11:33:02 -0500

    Donna Jarret updated fixed changed Shunya leex integration with MySQL c++ ruby integration db2

--- src/CrawlerThread.java
+++ src/CrawlerThread.java
@@ -4,12 +4,12 @@ public class CrawlerThread implements Runnable {
 
 	private Play play;
 	private static String url;
-	
+
 	public CrawlerThread(Play play) {
 		this.play = play;
 	}
-	
-	
+
+
 	@Override
 	public void run() {
 		if (main.DEBUG) {
@@ -20,9 +20,8 @@ public class CrawlerThread implements Runnable {
 		figureOutWhoToSendItTo(main.users, play.getKeywords()); //also calls send method
 
 	}
-	
-	
-	
+
+
 	public static void figureOutWhoToSendItTo(ArrayList<User> users, ArrayList<String> keywords) {
 		//given the associated keywords with the video, see if they match "tags" that one can subscribe to (either team name/players)
 		//and only send the message to people who are subscribed
@@ -32,6 +31,6 @@ public class CrawlerThread implements Runnable {
 			u.sendAlert(url);
 		}
 	}
-	
+
 
 }
