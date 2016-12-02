59598533a10672aef1772f837eadc5c3bd33637a
Kevin Chesser <kevin.chesser94@gmail.com>
2016-12-02 14:45:56 -0500

    add basic crawler to play

--- src/Play.java
+++ src/Play.java
@@ -1,5 +1,11 @@
+import java.io.IOException;
 import java.util.Date;
 import java.util.HashSet;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Play {
 
@@ -17,7 +23,19 @@ public class Play {
 	public String crawlSites() {
 		//see if any one video contains MOST of the keywords, and pick that video?
 		//validate url - make sure its a video
-
+		String redditURL = "http://www.reddit.com/r/soccer";
+		Document document = null;
+		try {
+			document = Jsoup.connect(redditURL).get(); //Get the url
+			Elements links = document.select("link[href]"); //Get the links from the url
+			System.out.println("Links: " + links.size());
+			for(Element link: links){
+				System.out.println("#Link: " + link.attr("abs:href") + " " + link.text());
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return "sampleUrl";
 	}
 
