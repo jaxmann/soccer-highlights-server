f51a9238d97463fb3cd12dfa854ef1391b0b5a85
Jonathan Axmann <jonathan.axmann09@gmail.com>
2017-04-11 09:14:01 -0400

    update logs and refresh rate calculation

--- src/CrawlerThread.java
+++ src/CrawlerThread.java
@@ -48,8 +48,6 @@ public class CrawlerThread implements Runnable {
 		
 		PropertyConfigurator.configure("log4j-configuration.txt"); //configure log4j binding with properties from log4j-configuration file
 	
-		
-
 		String redditURL = "http://www.reddit.com/r/soccer/new";
 		Document document = null;
 
@@ -58,27 +56,23 @@ public class CrawlerThread implements Runnable {
 		SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy");
 		formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
 
-		Pattern p = Pattern.compile("[0-9]+(-[0-9]+)");
-
-
+		Pattern p = Pattern.compile("[0-9]+(-[0-9]+)"); //does the link text have something like (2-0) displaying the score of a game ^[0-9]+(-[0-9]+)
+		
 		while (true) { //run forever unless stopped
 
 			try {
 				int refreshTime = getSleepTime();
-				logger.info("Current refresh rate: " + refreshTime / 1000 + " min");
+				logger.info("Current refresh rate: " + refreshTime / 60000 + " min");
 				Thread.sleep(refreshTime); //refresh page every n/1k seconds 
-				//modify this number to change based on time of the day/week/year potentially
-				//put a logger to print the refresh rate here
 			} catch (InterruptedException e2) {
-				e2.printStackTrace();
+				logger.error(e2.getMessage());
 			}
 
 			try {
 				document = Jsoup.connect(redditURL).userAgent(USER_AGENT).timeout(0).get(); //Get the url - Reddit only accepts 2 requests a minute. edit: 60/min i think? -jonathan
 				Elements links = document.select("div.thing"); //Get the entire posts from the doc
-				//System.out.println("Total Links: " + links.size());
+
 				logger.info("Most recent post time: " + mostRecentPostTime);
-				System.out.println(mostRecentPostTime);
 
 				for (Element link: links) {
 					String inputTime = link.select("p.tagline").select("time").attr("title");
@@ -86,18 +80,19 @@ public class CrawlerThread implements Runnable {
 						Date dateReddit = formatter.parse(inputTime);
 						if (mostRecentPostTime.compareTo(dateReddit) < 0) {
 							Matcher m = p.matcher(link.select("p.title").select("a.title").text());
-							//does the link text have something like (2-0) displaying the score of a game ^[0-9]+(-[0-9]+)
+							
 							if (m.find()) { // old .*(\\(|\\[)\\s?\\d{1}\\s?\\-\\s?\\d{1}\\s?(\\)|\\]).*
-								System.out.println("new post found");
 								String time = link.select("p.tagline").select("time").attr("title");
-								System.out.println(time); //time
 								String title = link.select("p.title").select("a.title").text();
-								System.out.println(title); //title
 								String url = link.select("p.title").select("a.title").attr("href");
-								System.out.println(url); //url
+
+								logger.info("New post found at " + time + " TITLE: " + title);
+								
 								mostRecentPostTime = formatter.parse(link.select("p.tagline").select("time").attr("title")); //update most recent post
 								String keyword = parseKeywords(title); //identify player keywords within play description
+								logger.info("Keyword is: " + keyword);
 								ArrayList<String> subbedUsers = findSubscribedUsers(keyword);
+								logger.info("Number of subbed users: " + subbedUsers.size());
 								if (subbedUsers.size() != 0) { //if no users are subscribed to a particular player, don't try to send email (it will fail)
 									sendEmail(url, keyword, subbedUsers); //send email to users who match keywords - send them the url, use keyword in email title/body; user's email is returned from sql query
 								}
@@ -105,53 +100,42 @@ public class CrawlerThread implements Runnable {
 							}
 						}
 					} catch (ParseException e1) {
-						e1.printStackTrace();
+						logger.error(e1.getMessage());
 					}	
 				}
 			} catch (IOException e) {
-				e.printStackTrace();
+				logger.error(e.getMessage());
 			}
 		}
 	}
 
 
-	public static String parseKeywords(String postDescription) { //should return ArrayList<String> edit: only if we find multiple keywords in a single play? not worth changing for now
+	public static String parseKeywords(String postDescription) { 
 		try {
-			BufferedReader reader = new BufferedReader (new FileReader("output.csv")); //backup list-of-players2
-			System.out.println("File found, trying to find player in play snippet");
+			BufferedReader reader = new BufferedReader (new FileReader("output.csv")); //backup version of this is "list-of-players2.csv"
 			String line;
+			
 			while ((line = reader.readLine()) != null) {
 				byte ptext[] = line.getBytes(ISO_8859_1);
 				String newline = new String(ptext, UTF_8);
 
 				String[] s = newline.split(",");
 				for (String a : s) {
-					//spaces are so that we actually find "Can" (on a word boundary) instead of Lezcano, for instance - not tested yet
+					//spaces are so that we actually find "Can" (on a word boundary) instead of Lezcano, for instance
 					if (postDescription.contains(" " + a + " ") || postDescription.contains(" " + simplify.simplifyName(a) + " ")) { //either ascii > 127 name or simplified name in play description? if yes...
-						System.out.println(s[0]);
-//						String firstHalf;
-//						if (postDescription.contains(a)) {
-//							firstHalf = postDescription.substring(0, postDescription.indexOf(a));
-//							while (parseKeywords(firstHalf) != null) {
-//								firstHalf = parseKeywords(firstHalf);
-//							}
-//						} else {
-//							firstHalf = postDescription.substring(0, postDescription.indexOf(simplify.simplifyName(a)));
-//						}
-						return s[0]; //found a keyword - we're done - technically should probably return the one that occurs first? idk
+						logger.info("Player found inside csv: " + s[0]);
+						return s[0]; 
 					}
 				}
-
-
 			}
+			
 			reader.close();
-			System.out.println("Done parsing for keywords.");
+			
 		} catch (Exception e) {
-			System.err.println("Error trying to read player file");
-			e.printStackTrace();
+			logger.error("Error trying to read player file");
 		}
-
-		return "poop"; //i.e no player found in the csv
+		
+		return "no-player-found"; //i.e no player found in the csv
 	}
 
 	public static ArrayList<String> findSubscribedUsers(String keyword) { 
@@ -165,18 +149,14 @@ public class CrawlerThread implements Runnable {
 			String url = "jdbc:sqlite:../server/db/pmr.db";
 			connection = DriverManager.getConnection(url);
 			long currentTime = System.nanoTime();
-			//note - this will get printed twice because i check it against null above
 			keyword = keyword.replace("'", "''");
 			String sql = "Select Email from User WHERE Keywords like '%" + keyword + "%' and ReceiveEmails<" + currentTime + ";";
-			System.out.println(sql);
+			logger.info("SQL: " + sql);
 			statement = connection.createStatement();
 			resultSet = statement.executeQuery(sql);
-			System.out.println("Connection successful");
+			//connection successful if error not caught below
 
 			//iterate over multiple results
-			
-			
-			//subscribedUsers.add(resultSet.getString("Email"));
 			if(resultSet.next()){
 				subscribedUsers.add(resultSet.getString("Email"));
 			}
@@ -185,7 +165,7 @@ public class CrawlerThread implements Runnable {
 			return subscribedUsers;
 
 		} catch (SQLException e){
-			System.out.println(e.getMessage());
+			logger.error(e.getMessage());
 		} finally {
 			try{
 				if (connection != null){
@@ -194,7 +174,7 @@ public class CrawlerThread implements Runnable {
 					connection.close();
 				}
 			} catch (SQLException ex) {
-				System.out.println(ex.getMessage());
+				logger.error(ex.getMessage());
 			}
 		}
 
@@ -203,44 +183,41 @@ public class CrawlerThread implements Runnable {
 
 	public static void sendEmail(String link, String keyword, ArrayList<String> emailAddresses) {
 
-		//add sendgrid key a file
+		//SG.txt and SGEmail.txt both need to be in home directory
 		String home = System.getProperty("user.home");
+		
 		byte[] encoded = null;
 		String pwd = "";
 		try {
 			encoded = Files.readAllBytes(Paths.get(home + "\\SG.txt"));
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			logger.error(e1.getMessage());
 		}
 		try {
 			pwd =  new String(encoded, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error(e.getMessage());
 		}
+		
 		byte[] encoded2 = null;
 		String pmremail = "";
 		try {
 			encoded = Files.readAllBytes(Paths.get(home + "\\SGEmail.txt"));
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			logger.error(e1.getMessage());
 		}
 		try {
 			pmremail =  new String(encoded, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error(e.getMessage());
 		}
 		
 		for (String em : emailAddresses) {
 			
-			System.out.println("email to send to " + em);
+			logger.info("Attempting to email: " + em + "...");
 			
 			Email from = new Email(pmremail); 
 			String subject = "PMR Highlight Found - " + keyword;
-			//Email to = new Email(email);
 			Email to = new Email(em);
 			Content content = new Content("text/plain", "Goal by " + keyword + "!" + " View (" + link + ").\n\n\n If this wasn't the correct player you selected, it's easiest just to uncheck that player"
 					+ " within the website - we're working on a solution to improve our app's cognitive ability. If you notice any other bugs feel free to send me an email personally at jonathan.axmann09@gmail.com");
@@ -255,21 +232,23 @@ public class CrawlerThread implements Runnable {
 				request.endpoint = "mail/send";
 				request.body = mail.build();
 				Response response = sg.api(request);
-				System.out.println(response.statusCode);
+				/*System.out.println(response.statusCode);
 				System.out.println(response.body);
-				System.out.println(response.headers);
+				System.out.println(response.headers);*/
+				logger.info("Email sent successfully");
+				logger.info(response);
 			} catch (IOException ex) {
-				ex.printStackTrace();
+				logger.error(ex.getMessage());
 			}
 		}
 	}
 	
-	public static int getSleepTime(){
+	public static int getSleepTime() {
 		Calendar calendar = Calendar.getInstance();
 		int hours = calendar.get(Calendar.HOUR_OF_DAY);
-		if(hours >= 19 && hours <= 6){ //7pm to 7am
+		if (hours >= 19 && hours <= 6) { //7pm to 7am
 			return 600000; //10 minutes
-		} else{
+		} else {
 			return 60000; //1 minute
 		}
 	}
