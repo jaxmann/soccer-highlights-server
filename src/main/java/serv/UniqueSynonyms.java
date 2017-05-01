c96224afd2b8ec0be8c04b28a38306b592c94a7b
Jonathan Axmann <jonathan.axmann09@gmail.com>
2017-05-01 13:51:07 -0400

    fix special characters in output.csv

--- src/main/java/serv/UniqueSynonyms.java
+++ src/main/java/serv/UniqueSynonyms.java
@@ -2,6 +2,9 @@ package serv;
 
 
 import java.io.BufferedReader;
+import static java.nio.charset.StandardCharsets.ISO_8859_1;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -14,7 +17,7 @@ public class UniqueSynonyms {
 		HashMap<String, Integer> myLastNames = new HashMap<String, Integer>();
 
 		try {
-			BufferedReader reader = new BufferedReader (new FileReader("list-of-players2.csv"));
+			BufferedReader reader = new BufferedReader (new FileReader("db//2bundes.csv"));
 			String line;
 
 			while ((line = reader.readLine()) != null) {
@@ -22,11 +25,12 @@ public class UniqueSynonyms {
 				//System.out.println(arr[0]);
 				String[] name = arr[0].split(" ");
 				if (name.length == 2) {
-					System.out.println(name[1]);
-					if (myLastNames.containsKey(name[1])) {
-						myLastNames.put(name[1], myLastNames.get(name[1]) + 1);
+					byte ptext[] = name[1].getBytes(ISO_8859_1);
+					String newline = new String(ptext, UTF_8);
+					if (myLastNames.containsKey(newline)) {
+						myLastNames.put(newline, myLastNames.get(newline) + 1);
 					} else {
-						myLastNames.put(name[1], 1);
+						myLastNames.put(newline, 1);
 					}
 					
 				} else {
@@ -42,7 +46,7 @@ public class UniqueSynonyms {
 		}
 		///////////////////////////////////////////////////////////////////////////////////////////////
 		try {
-			BufferedReader reader2 = new BufferedReader (new FileReader("list-of-players2.csv"));
+			BufferedReader reader2 = new BufferedReader (new FileReader("db//2bundes.csv"));
 			String line;
 
 			while ((line = reader2.readLine()) != null) {
@@ -53,15 +57,19 @@ public class UniqueSynonyms {
 				
 				String[] arr = line.split(",");
 				
-				String firstChunk = line;
+				byte ptext[] = line.getBytes(ISO_8859_1);
+				String newline = new String(ptext, UTF_8);
+				
+				String firstChunk = newline + ",";
 				
 				if (line.indexOf(",,") > 0) {
 					firstChunk = line.substring(0, line.indexOf(",,") + 1);
 				}
 				
+				byte ptext2[] = arr[0].getBytes(ISO_8859_1);
+				String newline2 = new String(ptext2, UTF_8);
 				
-				
-				String[] name = arr[0].split(" ");
+				String[] name = newline2.split(" ");
 				if (name.length == 2) {
 					if (myLastNames.containsKey(name[1])) {
 						if (myLastNames.get(name[1]) == 1) {
