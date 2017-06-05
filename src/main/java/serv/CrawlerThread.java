fb0e963d69fd8bfe31dec9cdbf321aa458bc57fa
jaxmann <jonathan.axmann09@gmail.com>
2017-06-04 20:28:28 -0400

    shorten sleeptime for test env

--- src/main/java/serv/CrawlerThread.java
+++ src/main/java/serv/CrawlerThread.java
@@ -406,20 +406,26 @@ public class CrawlerThread implements Runnable {
 		Calendar calendar = Calendar.getInstance();
 		int hours = calendar.get(Calendar.HOUR_OF_DAY);
 		int msWait = 60000;
+		
+		if (!redditenv.equals("test")) { //ie prod
+			if (hours >= 22 || hours <= 9) { //7pm to 6am
+				if (hours >= 22) {
+					msWait = 3600000*( (24-hours) + 9 );
+					return msWait; 
+				} else {
+					msWait = 3600000 * (9 - hours);
+					return msWait;
+				}
+				// milliseconds remaining until 6 am
 
-		if (hours >= 22 || hours <= 9) { //7pm to 6am
-			if (hours >= 22) {
-				msWait = 3600000*( (24-hours) + 9 );
-				return msWait; 
 			} else {
-				msWait = 3600000 * (9 - hours);
 				return msWait;
 			}
-			// milliseconds remaining until 6 am
-
 		} else {
-			return msWait;
+			return 60000;
 		}
+		
+		
 	}
 
 
