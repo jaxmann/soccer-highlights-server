7b8c07baebc1de00fc99460ff2d48b141c8d7537
jaxmann <jonathan.axmann09@gmail.com>
2017-06-10 14:56:09 -0400

    encoding test

--- src/main/java/serv/CrawlerThread.java
+++ src/main/java/serv/CrawlerThread.java
@@ -568,7 +568,7 @@ public class CrawlerThread implements Runnable {
 			while ((line = reader.readLine()) != null) {
 
 				byte pplayer[] = line.getBytes("Windows-1252");
-				String newplayer = new String(pplayer, UTF_8);
+				String newplayer = new String(pplayer, "Windows-1252");
 				
 				playerMatches.add(newplayer);
 
