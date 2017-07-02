9204d10f61ff25ae07df210d36dd1b182c62d0f5
jaxmann <jonathan.axmann09@gmail.com>
2017-07-02 09:17:24 -0400

    add imgtc as valid video site

--- src/main/java/serv/CrawlerThread.java
+++ src/main/java/serv/CrawlerThread.java
@@ -131,7 +131,7 @@ public class CrawlerThread implements Runnable {
 
 								mostRecentPostTime = formatter.parse(link.select("p.tagline").select("time").attr("title")); //update most recent post time
 
-								if (url.contains(".mp4") || url.contains("streamable")) { //only trigger if it's a video link
+								if (url.contains(".mp4") || url.contains("streamable") || url.contains("imgtc")) { //only trigger if it's a video link
 
 									logger.info("New post found: [" + title + "] at [" + time + "]");
 
