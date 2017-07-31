ee0152bb784d18e1d37e23d63432bbc3d8da5fa1
jaxmann <jonathan.axmann09@gmail.com>
2017-07-31 17:42:51 +0000

    update permissions for transfers

--- launch-test.sh
+++ launch-test.sh
@@ -1,1 +1,1 @@
-chmod 755 * && chmod 755 db/* &&  sudo ./gradlew run -Dexec.args="test"
+chmod 755 transfer.jar && chmod 777 regenerate-players/transfer-tmp.csv && chmod 755 * && chmod 755 db/* &&  sudo ./gradlew run -Dexec.args="test"
